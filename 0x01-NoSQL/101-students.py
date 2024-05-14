aa839b61170172e8cd14cd24c3b260d0d6bc9325
Rational-Being <overcomer021@gmail.com>
2024-05-14 20:38:10 +0100

    advanced task

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function returns all students sorted by score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    funtion returns all students sorted byaverage score
+    """
+    return mongo_collection.aggregate(
+        [
+            {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
+            {"$sort": {"avarageScore": -1}},
+        ]
+    )
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,8 +3,9 @@
 funtion returns the list of school having a specific topic
 """
 
+
 def schools_by_topic(mongo_collection, topic):
     """
     funtiion returns the list of school having a specific topic
     """
-    return mongo_collection.find({"topics": topic}) 
+    return mongo_collection.find({"topics": topic})
