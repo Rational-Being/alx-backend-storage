e87bb528384bf222c75903f33c367a86ecfd8b2f
Rational-Being <overcomer021@gmail.com>
2024-05-14 20:41:12 +0100

    advanced task

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -10,7 +10,8 @@ def top_students(mongo_collection):
     """
     return mongo_collection.aggregate(
         [
-            {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
+            {"$project": {"name": "$name", "averageScore": 
+            {"$avg": "$topics.score"}}},
             {"$sort": {"avarageScore": -1}},
         ]
     )
