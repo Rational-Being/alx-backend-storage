34d93637f68446082f400e80da63b3333b89acac
Rational-Being <overcomer021@gmail.com>
2024-05-14 18:04:27 +0100

    documentaiton

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-
+a funtion that ist all documents in a collection
 """
 import pymongo
 
+
 def list_all(mongo_collection):
     """
-
+    funtion lists all documents in a collection
     """
     return list(mongo_collection.find())
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-
+a funtion that inserts a new doument
 """
+
 
 def insert_school(mongo_collection, **kwargs):
     """
-
+    funtion inserts a new document
     """
     return mongo_collection.insert_one(kwargs).inserted_id
