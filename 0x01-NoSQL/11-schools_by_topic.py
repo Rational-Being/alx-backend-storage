f47e0bdc291f6484fa8d067c366b8058b4f4e053
Rational-Being <overcomer021@gmail.com>
2024-05-14 18:17:41 +0100

    advanced task

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+funtion returns the list of school having a specific topic
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    funtiion returns the list of school having a specific topic
+    """
+    return mongo_collection.find({"topics": topic}) 
