b66c4e43a063c4d32ebf20e9535762dbc1f08593
Rational-Being <overcomer021@gmail.com>
2024-05-15 21:22:40 +0100

    changed some typo

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -15,13 +15,16 @@ def wrap_request(method):
     """
     @wraps(method)
     def wrapper(url):
+
         cached_resp = rs.get("cached:" + url)
         if cached_resp:
             return cached_resp.decode("utf-8")
 
-        rs.incr(f"count:{url}")
-        rs.set(f"cached:{url}", method(url))
-        rs.expire(f"cached:{url}", 10)
+        # rs.incr(f"count:{url}")
+        # rs.set(f"cached:{url}", method(url))
+        # rs.expire(f"cached:{url}", 10)
+        rs.setex(f"cached:{url}", 10, method(url))
+
         return method(url)
     return wrapper
 
