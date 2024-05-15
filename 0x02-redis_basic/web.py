ffacbd0fb7eafb493370742da197a4d44c83e775
Rational-being <overcomer021@gmail.com>
2024-05-15 18:12:11 +0000

    advanced task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+import redis
+import requests
+from functools import wraps
+
+rs = redis.Redis()
+
+def wrap_request(method):
+    """
+
+    """
+    @wraps(method)
+    def wrapper(url):
+        cached_resp = rs.get("cached:" + url)
+        if cached_resp:
+            return cached_resp.decode("utf-8")
+
+        rs.incr(f"count:{url}")
+        rs.set(f"cached:{url}", method(url))
+        rs.expire(f"cached:{url}", 10)
+        return method(url)
+    return wrapper
+
+@wrap_request
+def get_page(url: str) -> str:
+    """
+
+    """
+    result = requests.get(url)
+    return result.text
